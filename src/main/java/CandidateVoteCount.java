270fe7287a4aa074b968748ed519f9992ccfa491
Mitia Jessica <mitiah.jessica@gmail.com>
2026-02-24 20:36:01 +0300

    add the class candidatevotecount

--- src/main/java/CandidateVoteCount.java
+++ src/main/java/CandidateVoteCount.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class CandidateVoteCount {
+    private String candidateName;
+    private int validVoteCount;
+
+    public CandidateVoteCount() {}
+
+    public String getCandidateName() {
+        return candidateName;
+    }
+
+    public void setCandidateName(String candidateName) {
+        this.candidateName = candidateName;
+    }
+
+    public int getValidVoteCount() {
+        return validVoteCount;
+    }
+
+    public void setValidVoteCount(int validVoteCount) {
+        this.validVoteCount = validVoteCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CandidateVoteCount that = (CandidateVoteCount) o;
+        return validVoteCount == that.validVoteCount && Objects.equals(candidateName, that.candidateName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(candidateName, validVoteCount);
+    }
+
+    @Override
+    public String toString() {
+        return  candidateName +
+                " = " + validVoteCount;
+    }
 }
