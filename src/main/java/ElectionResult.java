d97cb572ae0e05c17660bd3d237ce588dcf8f0b9
Mitia Jessica <mitiah.jessica@gmail.com>
2026-02-24 20:36:29 +0300

    add the class electionresult

--- src/main/java/ElectionResult.java
+++ src/main/java/ElectionResult.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class ElectionResult {
+    private String candidateName;
+    private int validVoteCount;
+
+    public String getCandidateName() {
+        return candidateName;
+    }
+
+    public void setCandidateName(String candidateName) {
+        this.candidateName = candidateName;
+    }
+
+    public int getValidVoteCount() {
+        return validVoteCount;
+    }
+
+    public void setValidVoteCount(int validVoteCount) {
+        this.validVoteCount = validVoteCount;
+    }
+
+    @Override
+    public String toString() {
+        return "ElectionResult{" +
+                "candidateName='" + candidateName + '\'' +
+                ", validVoteCount=" + validVoteCount +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ElectionResult that = (ElectionResult) o;
+        return validVoteCount == that.validVoteCount && Objects.equals(candidateName, that.candidateName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(candidateName, validVoteCount);
+    }
 }
