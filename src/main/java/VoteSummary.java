f44c45429a1e7511f7e28e68858d7eaad87f88be
Mitia Jessica <mitiah.jessica@gmail.com>
2026-02-24 20:35:35 +0300

    add the class votesummary

--- src/main/java/VoteSummary.java
+++ src/main/java/VoteSummary.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class VoteSummary {
+    private int validCount;
+    private int blankCount;
+    private int nullCount;
+
+    public VoteSummary() {}
+
+    @Override
+    public String toString() {
+        return "VoteSummary(" +
+                "validCount=" + validCount +
+                ", blankCount=" + blankCount +
+                ", nullCount=" + nullCount +
+                ')';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        VoteSummary that = (VoteSummary) o;
+        return validCount == that.validCount && blankCount == that.blankCount && nullCount == that.nullCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(validCount, blankCount, nullCount);
+    }
+
+    public int getValidCount() {
+        return validCount;
+    }
+
+    public void setValidCount(int validCount) {
+        this.validCount = validCount;
+    }
+
+    public int getBlankCount() {
+        return blankCount;
+    }
+
+    public void setBlankCount(int blankCount) {
+        this.blankCount = blankCount;
+    }
+
+    public int getNullCount() {
+        return nullCount;
+    }
+
+    public void setNullCount(int nullCount) {
+        this.nullCount = nullCount;
+    }
 }
