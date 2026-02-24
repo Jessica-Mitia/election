9ec792275e786d48582414764d48282286988ccf
Mitia Jessica <mitiah.jessica@gmail.com>
2026-02-24 20:37:53 +0300

    add the test in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.sql.SQLException;
 
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        DataRetriever data = new DataRetriever();
+
+        System.out.println(data.countAllVotes());
+        System.out.println(data.countVotesByType());
+        System.out.println(data.countValidVotesByCandidate());
+        System.out.println(data.computeVoteSummary());
+        System.out.println(data.findWinner());
+        System.out.println(data.computeTurnoutRate());
+    }
 }
