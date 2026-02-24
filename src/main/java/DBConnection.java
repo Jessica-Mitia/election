280f40daaac1dc3d54ef91d18628ac2a26195bda
Mitia Jessica <mitiah.jessica@gmail.com>
2026-02-24 20:34:19 +0300

    add the class dbConnection

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
+    private final Dotenv dotenv = Dotenv.load();
+
+    private String URL = dotenv.get("JDBC_URL");
+    private String USER = dotenv.get("USER");
+    private String PASSWORD = dotenv.get("PASSWORD");
+
+    public Connection getDBConnection() throws SQLException {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void closeDBConnection(Connection connection) throws SQLException {
+        if (connection != null) {
+            try {
+                connection.close();
+            }catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
+
